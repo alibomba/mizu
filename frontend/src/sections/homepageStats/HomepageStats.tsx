664ac2407106555bcd9d9ff4eea55a77bd3cd951
alibomba <wojci.bro@gmail.com>
2023-10-10 23:36:50 +0200

    stats

--- frontend/src/sections/homepageStats/HomepageStats.tsx
+++ frontend/src/sections/homepageStats/HomepageStats.tsx
@@ -4,7 +4,7 @@ import styles from './homepageStats.module.css';
 
 const HomepageStats = () => {
     return (
-        <section style={{ backgroundImage: `url('${process.env.REACT_APP_BACKEND_URL}/storage/assets/blob-bg-stats.svg')` }} className={styles.stats}>
+        <section className={styles.stats}>
 
         </section>
     )

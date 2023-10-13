951e57f0246e221a8012f94818c89ceac9a39a09
alibomba <wojci.bro@gmail.com>
2023-10-13 15:07:28 +0200

    projekty i aktualnosci

--- backend/routes/projectRoutes.ts
+++ backend/routes/projectRoutes.ts
@@ -1,5 +1,6 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Project } from '@prisma/client';
 import { Request, Response, Router } from 'express';
+import { PaginationResponse } from '../types/PaginationResponse';
 
 const projectRoutes: Router = Router();
 
@@ -18,6 +19,35 @@ projectRoutes.get('/nearest-projects', async (req: Request, res: Response) => {
     } else {
         res.status(404).json({ message: 'Za mało projektów' });
     }
+});
+
+const PER_PAGE = 8;
+
+projectRoutes.get('/projects', async (req: Request, res: Response) => {
+    let page = req.query.page as string | number;
+    if (page) page = +page;
+    else page = 1;
+    const projectCount = await prisma.project.count();
+    const lastPage = Math.ceil(projectCount / PER_PAGE);
+    if (page > lastPage) return res.status(404).json({ message: `Jest tylko ${lastPage} stron` });
+    const offset = (page - 1) * PER_PAGE;
+    const projects = await prisma.project.findMany({
+        take: PER_PAGE,
+        skip: offset
+    });
+    const response: PaginationResponse<Project> = {
+        currentPage: page,
+        lastPage,
+        data: projects
+    };
+    res.json(response);
+});
+
+projectRoutes.get('/project/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const project = await prisma.project.findUnique({ where: { id } });
+    if (!project) return res.status(404).json({ message: 'Nie znaleziono projektu' });
+    res.json(project);
 });
 
 
--- frontend/src/pages/about/About.tsx
+++ frontend/src/pages/about/About.tsx
@@ -4,8 +4,54 @@ import styles from './about.module.css';
 
 const About = () => {
     return (
-        <>
-        </>
+        <main className={styles.main}>
+            <section className={styles.section}>
+                <div className={styles.section__content}>
+                    <h2 className={styles.section__heading}>Nasz cel</h2>
+                    <p className={styles.section__text}>Nasz główny cel jest prosty, ale głęboko zakorzeniony w naszej misji: chcemy chronić i zachować nasze środowisko dla teraźniejszych i przyszłych pokoleń. Dążymy do stworzenia zrównoważonej przyszłości, w której ludzie i natura współistnieją harmonijnie.</p>
+                </div>
+                <img className={styles.section__img} src={`${process.env.REACT_APP_BACKEND_URL}/storage/assets/o-nas-nasz-cel.jpg`} alt="grupa młodych ludzi" />
+            </section>
+            <section className={styles.section}>
+                <img className={styles.section__img} src={`${process.env.REACT_APP_BACKEND_URL}/storage/assets/o-nas-nasze-wartosci.jpg`} alt="biurko z laptopem i kalendarzem" />
+                <div className={styles.section__content}>
+                    <h2 className={styles.section__heading}>Nasze wartości</h2>
+                    <p className={styles.section__text}>Naszą organizację kierują fundamentalne wartości: poszanowanie przyrody, zrównoważony rozwój, edukacja ekologiczna i współpraca społeczna. Te wartości stanowią fundament naszych działań i inspirują naszą misję ochrony środowiska.</p>
+                </div>
+            </section>
+            <section className={styles.team}>
+                <article className={styles.team__person}>
+                    <img className={styles.person__img} src={`${process.env.REACT_APP_BACKEND_URL}/storage/assets/team/anna-kowalska.jpg`} alt="Anna Kowalska" />
+                    <p className={styles.person__name}>Anna Kowalska</p>
+                    <p className={styles.person__role}>Ekspert ds. Edukacji Ekologicznej</p>
+                </article>
+                <article className={styles.team__person}>
+                    <img className={styles.person__img} src={`${process.env.REACT_APP_BACKEND_URL}/storage/assets/team/anna-kowalska.jpg`} alt="Anna Kowalska" />
+                    <p className={styles.person__name}>Anna Kowalska</p>
+                    <p className={styles.person__role}>Ekspert ds. Edukacji Ekologicznej</p>
+                </article>
+                <article className={styles.team__person}>
+                    <img className={styles.person__img} src={`${process.env.REACT_APP_BACKEND_URL}/storage/assets/team/anna-kowalska.jpg`} alt="Anna Kowalska" />
+                    <p className={styles.person__name}>Anna Kowalska</p>
+                    <p className={styles.person__role}>Ekspert ds. Edukacji Ekologicznej</p>
+                </article>
+                <article className={styles.team__person}>
+                    <img className={styles.person__img} src={`${process.env.REACT_APP_BACKEND_URL}/storage/assets/team/anna-kowalska.jpg`} alt="Anna Kowalska" />
+                    <p className={styles.person__name}>Anna Kowalska</p>
+                    <p className={styles.person__role}>Ekspert ds. Edukacji Ekologicznej</p>
+                </article>
+                <article className={styles.team__person}>
+                    <img className={styles.person__img} src={`${process.env.REACT_APP_BACKEND_URL}/storage/assets/team/anna-kowalska.jpg`} alt="Anna Kowalska" />
+                    <p className={styles.person__name}>Anna Kowalska</p>
+                    <p className={styles.person__role}>Ekspert ds. Edukacji Ekologicznej</p>
+                </article>
+                <article className={styles.team__person}>
+                    <img className={styles.person__img} src={`${process.env.REACT_APP_BACKEND_URL}/storage/assets/team/anna-kowalska.jpg`} alt="Anna Kowalska" />
+                    <p className={styles.person__name}>Anna Kowalska</p>
+                    <p className={styles.person__role}>Ekspert ds. Edukacji Ekologicznej</p>
+                </article>
+            </section>
+        </main>
     )
 }
 
